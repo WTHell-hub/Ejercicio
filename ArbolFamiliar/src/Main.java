da82c85eba150e14e7bf325905beb5359c914572
WTHell <wandilieltorreshernandez2005@gmail.com>
2026-03-12 16:11:40 -0400

    errores pendientes

--- ArbolFamiliar/src/Main.java
+++ ArbolFamiliar/src/Main.java
@@ -1,51 +1,51 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("SISTEMA ARBOL GENEALOGICO\n");
+        System.out.println("SISTEMA ARBOL GENEALOGICO - FAMILIA RODRÍGUEZ\n");
 
-        ArbolGenealogico fam = new ArbolGenealogico();
-
-
-        fam.addRoot("Carlos Ruiz", "15/03/1950", 'M', 3);
-
-        fam.addChild("Carlos Ruiz", "Ana Ruiz", "20/07/1975", 'F', 2);
-        fam.addChild("Carlos Ruiz", "Luis Ruiz", "10/11/1978", 'M', 3);
-        fam.addChild("Carlos Ruiz", "Marta Ruiz", "05/02/1982", 'F', 2);
-
-        fam.addChild("Ana Ruiz", "Pedro Ruiz", "12/04/2000", 'M', 0);
-        fam.addChild("Ana Ruiz", "Sofia Ruiz", "25/09/2003", 'F', 0);
-
-        fam.addChild("Luis Ruiz", "Diego Ruiz", "30/06/2005", 'M', 0);
-        fam.addChild("Luis Ruiz", "Valentina Ruiz", "18/01/2008", 'F', 0);
-        fam.addChild("Luis Ruiz", "Javier Ruiz", "22/11/2010", 'M', 0);
-
-        fam.addChild("Marta Ruiz", "Camila Ruiz", "07/03/2012", 'F', 0);
-        fam.addChild("Marta Ruiz", "Nicolas Ruiz", "19/08/2015", 'M', 0);
-
-        fam.addChild("Diego Ruiz", "Lucia Ruiz", "10/05/2030", 'F', 0);
+        ArbolGenealogico familia = new ArbolGenealogico();
 
 
-        fam.showTree();
-        fam.recorridoXGen();
-        fam.showDepth();
+        familia.addRoot("José Rodríguez", "15/03/1950", 'M', 3);
 
-        fam.showAncestros("Diego Ruiz");
-        fam.showAncestros("Lucia Ruiz");
-        fam.showAncestros("Carlos Ruiz");
-        fam.showAncestros("Xxx Xxx");
+        familia.addChild("José Rodríguez", "María Rodríguez", "20/07/1975", 'F', 2);
+        familia.addChild("José Rodríguez", "Carlos Rodríguez", "10/11/1978", 'M', 3);
+        familia.addChild("José Rodríguez", "Ana Rodríguez", "05/02/1982", 'F', 2);
 
-        fam.showDescendientes("Luis Ruiz");
-        fam.showDescendientes("Ana Ruiz");
-        fam.showDescendientes("Diego Ruiz");
+        familia.addChild("María Rodríguez", "Pedro Rodríguez", "12/04/2000", 'M', 0);
+        familia.addChild("María Rodríguez", "Lucía Rodríguez", "25/09/2003", 'F', 0);
+
+        familia.addChild("Carlos Rodríguez", "Andrés Rodríguez", "30/06/2005", 'M', 0);
+        familia.addChild("Carlos Rodríguez", "Valentina Rodríguez", "18/01/2008", 'F', 0);
+        familia.addChild("Carlos Rodríguez", "Mateo Rodríguez", "22/11/2010", 'M', 0);
+
+        familia.addChild("Ana Rodríguez", "Camila Rodríguez", "07/03/2012", 'F', 0);
+        familia.addChild("Ana Rodríguez", "Nicolás Rodríguez", "19/08/2015", 'M', 0);
+
+        familia.addChild("Andrés Rodríguez", "Sofía Rodríguez", "10/05/2030", 'F', 0);
+
+
+        familia.showTree();
+        familia.recorridoXGen();
+        familia.showDepth();
+
+        familia.showAncestros("Andrés Rodríguez");
+        familia.showAncestros("Sofía Rodríguez");
+        familia.showAncestros("José Rodríguez");
+        familia.showAncestros("Xxx Xxx");
+
+        familia.showDescendientes("Carlos Rodríguez");
+        familia.showDescendientes("María Rodríguez");
+        familia.showDescendientes("Andrés Rodríguez");
 
         System.out.println("\nPRUEBA BORRADO");
 
-        fam.deleteBranch("Marta Ruiz");
+        familia.deleteBranch("Ana Rodríguez");
 
-        System.out.println("\nDespues de borrar:");
-        fam.showTree();
-        fam.showDepth();
+        System.out.println("\nDespués de borrar:");
+        familia.showTree();
+        familia.showDepth();
 
-        fam.deleteBranch("Camila Ruiz");
+        familia.deleteBranch("Camila Rodríguez");
     }
 }
